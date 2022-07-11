18c27da362d1751d3777ed928d40748f8441ab20
Dhruv Manilawala <dhruvmanila@gmail.com>
2022-07-11 21:09:06 +0530

    fix: rename to avoid redefining builtin `dir`
    fixes: #13

--- tests/test_e2e.py
+++ tests/test_e2e.py
@@ -18,8 +18,7 @@ def test_verbose_output(tmp_path: str) -> None:
     runner = CliRunner()
     newlines = 10
     with runner.isolated_filesystem(tmp_path) as temp_dir:
-        dir = Path(temp_dir)
-        dir.joinpath("hello.py").write_text(
+        Path(temp_dir).joinpath("hello.py").write_text(
             'print("hello")' + "\n" * newlines + 'print("world")'
         )
         result = runner.invoke(main, ["hello.py", "--verbose"])
@@ -36,9 +35,9 @@ def test_ignore_single_files(tmp_path: str) -> None:
     content = "print('hello')\n"
 
     with runner.isolated_filesystem(tmp_path) as temp_dir:
-        dir = Path(temp_dir)
+        dirpath = Path(temp_dir)
         for filename in filenames:
-            dir.joinpath(filename).write_text(content)
+            dirpath.joinpath(filename).write_text(content)
         result = runner.invoke(main, ["--ignore", "hello2.py", *filenames])
 
     assert result.exit_code == 0
@@ -46,7 +45,7 @@ def test_ignore_single_files(tmp_path: str) -> None:
     assert result.stdout_bytes
     assert "2 files transformed" in result.output
     assert "2 print statements removed" in result.output
-    assert content in dir.joinpath(filenames[1]).read_text()
+    assert content in dirpath.joinpath(filenames[1]).read_text()
 
 
 def test_ignore_multiple_files(tmp_path: str) -> None:
@@ -55,9 +54,9 @@ def test_ignore_multiple_files(tmp_path: str) -> None:
     content = "print('hello')\n"
 
     with runner.isolated_filesystem(tmp_path) as temp_dir:
-        dir = Path(temp_dir)
+        dirpath = Path(temp_dir)
         for filename in filenames:
-            dir.joinpath(filename).write_text(content)
+            dirpath.joinpath(filename).write_text(content)
         result = runner.invoke(
             main, ["--ignore", "hello2.py", "--ignore", "hello3.py", *filenames]
         )
@@ -67,8 +66,8 @@ def test_ignore_multiple_files(tmp_path: str) -> None:
     assert result.stdout_bytes
     assert "1 file transformed" in result.output
     assert "1 print statement removed" in result.output
-    assert content in dir.joinpath(filenames[1]).read_text()
-    assert content in dir.joinpath(filenames[2]).read_text()
+    assert content in dirpath.joinpath(filenames[1]).read_text()
+    assert content in dirpath.joinpath(filenames[2]).read_text()
 
 
 def test_no_print_statements(tmp_path: str) -> None:
@@ -77,9 +76,9 @@ def test_no_print_statements(tmp_path: str) -> None:
     content = "a = 5\n"
 
     with runner.isolated_filesystem(tmp_path) as temp_dir:
-        dir = Path(temp_dir)
+        dirpath = Path(temp_dir)
         for filename in filenames:
-            dir.joinpath(filename).write_text(content)
+            dirpath.joinpath(filename).write_text(content)
         result = runner.invoke(main, filenames)
 
     assert result.exit_code == 0
@@ -91,8 +90,7 @@ def test_no_print_statements(tmp_path: str) -> None:
 def test_transform_failure(tmp_path: str) -> None:
     runner = CliRunner()
     with runner.isolated_filesystem(tmp_path) as temp_dir:
-        dir = Path(temp_dir)
-        dir.joinpath("hello.py").write_text('print("hello"\n')
+        Path(temp_dir).joinpath("hello.py").write_text('print("hello"\n')
         result = runner.invoke(main, ["hello.py"])
 
     assert result.exit_code == 123
